08646c35f97c1a73f94b17a16ab8a95e4608701e
Camille Emmer <camilleemmer@gmail.com>
2022-07-21 15:48:06 -0600

    commit tree e99eb650ff2046dfc97222914e70e07a04d0eb7d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let day = document.getElementById('main')
+day.remove(main);
+
+const newHeader = document.createElement('h1')      
+newHeader.id ='victory';
+
+newHeader.textContent =  "Camille is the champion"
+document.body.append(newHeader)
